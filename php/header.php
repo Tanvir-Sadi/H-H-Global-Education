badd82ff3f8cb0ff24c7e3cd84ee4bb801276a74
tanvir-sadi <tanvir.sadi10@northsouth.edu>
2021-06-07 23:08:37 +0600

    Header file initial

---
+++ php/header.php
@@ -0,0 +1,13 @@
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <script src="https://kit.fontawesome.com/74fd6a799b.js" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="css/style.css">
+  <title>Hello, world!</title>
+</head>
