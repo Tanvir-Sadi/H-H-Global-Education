48a7330abbfff195509d11482c7bffb67f87aaa5
tanvir-sadi <tanvir.sadi10@northsouth.edu>
2021-06-17 22:03:51 +0600

    Update: global branches, navbar link added, fast-facts removed: mouse scrolling js, hasin page

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ owl.owlCarousel({
     loop:true,
     margin:10,
     responsiveClass:true,
-    // autoplay: true,
+    autoplay: true,
     responsive:{
         0:{
             items:1,
@@ -13,13 +13,4 @@ owl.owlCarousel({
             items:2,
         }
     }
-});
-
-owl.on('mousewheel', '.owl-stage', function (e) {
-    if (e.deltaY>0) {
-        owl.trigger('next.owl');
-    } else {
-        owl.trigger('prev.owl');
-    }
-    e.preventDefault();
 });
