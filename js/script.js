3cbdd906c29a2c6c4b5b1e65ca3c765ffe0cfe32
tanvir-sadi <tanvir.sadi10@northsouth.edu>
2021-06-23 14:23:38 +0600

    Fixed: SVG error, Form error

--- js/script.js
+++ js/script.js
@@ -2,14 +2,13 @@ var owl = $('.owl-carousel');
 
 owl.owlCarousel({
     loop:true,
-    margin:10,
     responsiveClass:true,
     autoplay: true,
     responsive:{
         0:{
             items:1,
         },
-        950:{
+        1000:{
             items:2,
         }
     }
--- php/footer.php
+++ php/footer.php
@@ -15,73 +15,63 @@
         <ul class="my-5">
           <li class="my-1">
             <div class="">
-              <i class="fab fa-facebook-f m-2"></i>
-              <span>Head Office: Unit 107, 8-10 Greatorex Street London E1 5NF London, UK</span>
+              <i class="fas fa-map-marker-alt m-2"></i>
+              <span><span class="fw-bold">Head Office:</span> Unit 107, 8-10 Greatorex Street London E1 5NF London, UK</span>
             </div>
           </li>
           <li class="my-1">
             <div class="">
-              <i class="fab fa-facebook-f m-2"></i>
-              <span>44-7399-599646, 44-7969-553031 [Direct; WhatsApp]</span>
+              <i class="fab fa-whatsapp-square m-2"></i>
+              <span><span class="fw-bold">WhatsApp:</span> +44-7399-599646, +44-7969-553031</span>
             </div>
           </li>
           <li class="my-1">
             <div class="">
-              <i class="fab fa-facebook-f m-2"></i>
-              <span>info@hhglobaledu.co.uk</span>
+              <i class="fas fa-envelope m-2"></i>
+              <span><span class="fw-bold">Email:</span> info@hhglobaledu.co.uk</span>
             </div>
           </li>
         </ul>
         <div>
           <a class="social" href="#"><i class="fab fa-facebook-f"></i></a>
+          <a class="social" href="#"><i class="fab fa-twitter"></i></a>
           <a class="social" href="#"><i class="fab fa-linkedin-in"></i></a>
-          <a class="social" href="#"><i class="fab fa-facebook-f"></i></a>
         </div>
       </div>
       <div class="col-sm-12 col-md-6 my-5">
         <div class="row">
           <div class="col mb-3">
-              <h5>Services</h5>
-              <ul>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Pricing</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
+            <h6>Services</h6>
+            <ul>
+              <li class="text-decoration-underline"><a href="#">Student Bank Accounts</a></li>
+              <li class="text-decoration-underline"><a href="#">Free Assessment</a></li>
+              <li class="text-decoration-underline"><a href="#">Visa Services</a></li>
+              <li class="text-decoration-underline"><a href="#">Airport Pickup</a></li>
+              <li class="text-decoration-underline"><a href="#">Student Travel Card</a></li>
+              <li class="text-decoration-underline"><a href="#">Branches</a></li>
+              <li class="text-decoration-underline"><a href="#">Events</a></li>
+            </ul>
           </div>
           <div class="col mb-3">
-              <h5>Services</h5>
-              <ul>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Pricing</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
+            <h6>UK Study</h6>
+            <ul>
+              <li class="text-decoration-underline"><a href="#">Courses</a></li>
+              <li class="text-decoration-underline"><a href="#">UK University</a></li>
+              <li class="text-decoration-underline"><a href="#">Application Process</a></li>
+              <li class="text-decoration-underline"><a href="#">Scholarship</a></li>
+              <li class="text-decoration-underline"><a href="#">Help & Support</a></li>
+            </ul>
           </div>
           <div class="col mb-3">
-              <h5>Services</h5>
-              <ul>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Pricing</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
+            <h6>Quick Links</h6>
+            <ul>
+              <li class="text-decoration-underline"><a href="#">About</a></li>
+              <li class="text-decoration-underline"><a href="#">Our Support</a></li>
+              <li class="text-decoration-underline"><a href="#">Offices</a></li>
+            </ul>
           </div>
         </div>
 
-        <!-- <div class="text-end my-3">
-          <h5>Subscribe to our Newsletter</h5>
-          <form class="row justify-content-between" action="index.html" method="post">
-            <div class="form-floating col-9 mb-3">
-              <input type="email" class="form-control bg-light" id="floatingInput" placeholder="name@example.com">
-              <label for="floatingInput">Email address</label>
-            </div>
-            <button class="btn __custom-gradient text-light col-2 px-3 py-2">Submit</button>
-          </form>
-        </div> -->
         <div class="d-flex justify-content-center">
           <form class="my-lg-5 my-0 rounded col-10 row d-flex justify-content-end">
             <!-- Email input -->
@@ -95,14 +85,14 @@
               <hr>
             </div>
             <!-- Submit button -->
-            <div class="col-12 col-sm-6 col-lg-3 p-0 my-4 ">
+            <div class="col-12 col-sm-6 col-lg-3 p-0 my-2 ">
               <button type="submit" class="btn __color-background-red btn-block text-white __font-400">Subscribe</button>
             </div>
           </form>
         </div>
 
-        </div>
       </div>
     </div>
+  </div>
   </div>
 </footer>
